13693b0a71977fd96d6fa687dee381bb713fa041
Osman Coskun <osman.coskun95@gmail.com>
2023-05-11 09:09:04 +0300

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,9 @@
+<script context="module">
+    import "./App.css";
+    import { Router, createRouter } from "@roxi/routify";
+    import routes from "../.routify/routes.default.js";
+
+    const router = createRouter({ routes });
+</script>
+
+<Router {router} />
---
+++ src/routes/index.svelte
@@ -0,0 +1,6 @@
+<main
+    class="h-screen w-screen flex flex-col gap-y-5 items-center justify-center"
+>
+    <a href="#" class="font-bold text-2xl"> ROUTIFY + SVELTE + MARKDOWN </a>
+    <a href="/hello-world">Hello World markdown page</a>
+</main>
